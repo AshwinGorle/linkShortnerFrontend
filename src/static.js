668e91a7e46cbc8bc0469360ffdaeeb0fd25d84b
Ashwin Gorle <ashwingorle7777@gmail.com>
2024-02-07 20:34:55 +0530

    updated env

--- src/static.js
+++ src/static.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://linkshortner-rra9.onrender.com";
-export const BASE_URL = "http://localhost:3000";
+ export const BASE_URL = "https://linkshortner-rra9.onrender.com";
+//export const BASE_URL = "http://localhost:3000";
